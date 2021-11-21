780fc48eb10ae6e9945dfcd4006c41515410c4f9
juancruzcastro <juaancastro98@gmail.com>
2021-11-21 17:55:23 -0300

    Add: Creación del servidor con NodeJS.

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json()); // convierto todo lo que llegue a json
 // CORS
 
 // rutas
+app.get('/', (req, res) => {
+    // la req = request, son los datos que envia el cliente y la res = response, los datos que envío
+    res.status(200).send(
+        "<h1>Página de inicio</h1>"
+    );
+});
 
 // exportar este modulo
 module.exports = app;
